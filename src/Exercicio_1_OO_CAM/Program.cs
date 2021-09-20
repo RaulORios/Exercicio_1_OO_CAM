ec8e527c432ff00460ba0f797c3ad45a7d0e3b28
RaulORios <raul.orios89@outlook.com>
2021-09-20 14:57:23 -0300

    Arquivos adicionados

---
+++ src/Exercicio_1_OO_CAM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1_OO_CAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a Largura e Altura do Retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"AREA = {r.Area().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {r.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {r.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ src/Exercicio_1_OO_CAM/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1_OO_CAM
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+
+        //public override string ToString()
+        //{
+        //    return ($"AREA = {Area().ToString("F2", CultureInfo.InvariantCulture)}");
+        //}
+
+    }
+}
